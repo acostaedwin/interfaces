98853f4c40cbe4614cce2082aee1d031f319e49c
Edwin <edwin00000@hotmail.com>
2021-11-24 00:48:41 +0000

    add example pages

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+/* Using with useIonActionSheet Hook */
+
+import React from 'react';
+import {
+  IonButton,
+  IonContent,
+  IonPage,
+  useIonActionSheet,
+} from '@ionic/react';
 
 const Tab1: React.FC = () => {
+  const [present, dismiss] = useIonActionSheet();
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+        <IonButton
+          expand="block"
+          onClick={() =>
+            present({
+              buttons: [{ text: 'Ok' }, { text: 'Cancel' }],
+              header: 'Action Sheet'
+            })
+          }
+        >
+          Show ActionSheet
+        </IonButton>
+        <IonButton
+          expand="block"
+          onClick={() =>
+            present([{ text: 'Ok' }, { text: 'Cancel' }], 'Action Sheet')
+          }
+        >
+          Show ActionSheet using params
+        </IonButton>
+        <IonButton
+          expand="block"
+          onClick={() => {
+            present([{ text: 'Ok' }, { text: 'Cancel' }], 'Action Sheet');
+            setTimeout(dismiss, 3000);
+          }}
+        >
+          Show ActionSheet, hide after 3 seconds
+        </IonButton>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,25 +1,59 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import React from 'react';
 
-const Tab3: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+import { IonButton, IonIcon, IonContent } from '@ionic/react';
+import { star } from 'ionicons/icons';
+
+export const Tab3: React.FC = () => (
+  <IonContent>
+    {/*-- Default --*/}
+    <IonButton>Default</IonButton>
+
+    {/*-- Anchor --*/}
+    <IonButton href="#">Anchor</IonButton>
+
+    {/*-- Colors --*/}
+    <IonButton color="primary">Primary</IonButton>
+    <IonButton color="secondary">Secondary</IonButton>
+    <IonButton color="tertiary">Tertiary</IonButton>
+    <IonButton color="success">Success</IonButton>
+    <IonButton color="warning">Warning</IonButton>
+    <IonButton color="danger">Danger</IonButton>
+    <IonButton color="light">Light</IonButton>
+    <IonButton color="medium">Medium</IonButton>
+    <IonButton color="dark">Dark</IonButton>
+
+    {/*-- Expand --*/}
+    <IonButton expand="full">Full Button</IonButton>
+    <IonButton expand="block">Block Button</IonButton>
+
+    {/*-- Round --*/}
+    <IonButton shape="round">Round Button</IonButton>
+
+    {/*-- Fill --*/}
+    <IonButton expand="full" fill="outline">Outline + Full</IonButton>
+    <IonButton expand="block" fill="outline">Outline + Block</IonButton>
+    <IonButton shape="round" fill="outline">Outline + Round</IonButton>
+
+    {/*-- Icons --*/}
+    <IonButton>
+      <IonIcon slot="start" icon={star} />
+      Left Icon
+    </IonButton>
+
+    <IonButton>
+      Right Icon
+      <IonIcon slot="end" icon={star} />
+    </IonButton>
+
+    <IonButton>
+      <IonIcon slot="icon-only" icon={star} />
+    </IonButton>
+
+    {/*-- Sizes --*/}
+    <IonButton size="large">Large</IonButton>
+    <IonButton>Default</IonButton>
+    <IonButton size="small">Small</IonButton>
+  </IonContent>
+);
 
 export default Tab3;
